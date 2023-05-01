bb858df16bb68e94d8e4a9a4bb50681c7e62c875
Sin Xu <31815270+SinXu@users.noreply.github.com>
2023-05-01 17:50:31 +0800

    更新

--- pages/_document.js
+++ pages/_document.js
@@ -15,13 +15,6 @@ class MyDocument extends Document {
         <Head>
           <link rel='icon' href='/favicon.ico' />
           <CommonScript />
-          <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "gum1rdsjc1");
-</script>
         </Head>
 
         <body className={`${BLOG.FONT_STYLE} font-light bg-day dark:bg-night`}>
